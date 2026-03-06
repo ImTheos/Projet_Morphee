32e2930fa0eb8054dbb4398b54c45757f1fb0a3b
Mathieu Simon <matt.simon977@gmail.com>
2026-03-06 16:55:19 +0100

    New cursor + fixed dash

--- Source/Projet_Morphee/Private/Input/Cursor.cpp
+++ Source/Projet_Morphee/Private/Input/Cursor.cpp
@@ -26,6 +26,14 @@ FVector ACursor::GetCursorWorldPosition()
 	{
 		return FVector::Zero();
 	}
+	
+	FString debugString = FString::Printf(TEXT("Magnitude : %f"), mouseDirection.Length());
+	GEngine->AddOnScreenDebugMessage(111, 1.0f, FColor::Yellow, debugString);
+	
+	if (mouseDirection.IsNearlyZero())
+	{
+		return FVector::Zero();
+	}
 
 	const double playerHeight = playerCharacter->GetActorLocation().Z - playerCharacter->GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
 
